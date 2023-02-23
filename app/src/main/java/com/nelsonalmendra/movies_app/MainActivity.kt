34e34ce88563fb4fe1f43fc32421c2b8f1f78fcf
Nelson Almendra <nelsonalmendra@gmail.com>
2023-02-23 22:57:51 +0000

    Initial commit

---
+++ app/src/main/java/com/nelsonalmendra/movies_app/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.nelsonalmendra.movies_app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.nelsonalmendra.movies_app.ui.theme.MoviesappTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MoviesappTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MoviesappTheme {
+        Greeting("Android")
+    }
+}
