8204eddb38e5ae594a0431fe41c1668e0055f0f6
Nelson Almendra <nelsonalmendra@gmail.com>
2023-03-19 00:51:02 +0000

    Add initial screens for search

--- app/src/main/java/com/nelsonalmendra/movies/MainActivity.kt
+++ app/src/main/java/com/nelsonalmendra/movies/MainActivity.kt
@@ -3,14 +3,24 @@ package com.nelsonalmendra.movies
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.nelsonalmendra.movies.ui.theme.MoviesappTheme
 import com.nelsonalmendra.movies.viewmodels.MoviesViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,12 +31,12 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MoviesappTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+                    MoviesNavHost(navController)
                 }
             }
         }
@@ -34,9 +44,60 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
+fun MoviesNavHost(navController: NavHostController) {
+    NavHost(
+        navController = navController,
+        startDestination = "home"
+    ) {
+        composable("home") {
+            HomeScreen {
+                navController.navigate("search")
+            }
+        }
+        composable("search") { SearchScreen() }
+    }
+}
+
+@Composable
+fun HomeScreen(onButtonClick: () -> Unit = {}) {
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)
+    ) {
+        OutlinedButton (
+            onClick = onButtonClick,
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Search,
+                modifier = Modifier.defaultMinSize(),
+                contentDescription = "search")
+
+            Text(
+                text = "Search for a movie..."
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()) {
+        val searchText = remember { mutableStateOf(TextFieldValue()) }
+        TextField(value = searchText.value, onValueChange = {
+            searchText.value = it
+        })
+    }
+}
+
+@Composable
 fun Greeting(
     name: String,
     viewModel: MoviesViewModel = hiltViewModel()) {
+//    viewModel.test()
     Text(text = "Hello $name!")
 }
 
@@ -44,6 +105,12 @@ fun Greeting(
 @Composable
 fun DefaultPreview() {
     MoviesappTheme {
-        Greeting("Android")
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+//            HomeScreen()
+            SearchScreen()
+        }
     }
 }
